9f87767572d75d7334e98cd4ecf50706f1987ce8
Balaji2624 <balajisapkal3062001@gmail.com>
2024-08-31 19:34:20 +0530

    welcome

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmpWagesPrograms
+# EmpWagesPrograms
---
+++ src/EmpWages/Welcome.java
@@ -0,0 +1,7 @@
+package EmpWages;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
