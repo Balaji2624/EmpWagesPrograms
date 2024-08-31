7ffdfd75f20a894d399e633c4a16fe9f34304656
Balaji2624 <balajisapkal3062001@gmail.com>
2024-08-31 19:41:26 +0530

    Employee is Present or NOT

---
+++ src/EmpWages/EmpPresence.java
@@ -0,0 +1,17 @@
+package EmpWages;
+
+public class EmpPresence {
+    public static void main(String[] args) {
+
+        int isFullTime=1;
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if(empCheck==isFullTime)
+        {
+            System.out.println("Employee is Present..");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+    }
+}
+
